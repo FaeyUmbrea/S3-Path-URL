7d48ef7fbd7849b349e0885c89aa1e47e4754f44
Faey Umbrea <faey@void.monster>
2022-05-08 06:11:33 +0200

    Initial Try

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,16 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+Hooks.on('renderApplication', (app, html, data) => {
+    if(typeof app === FilePicker){
+        if(app._tabs[0].active === 's3'){
+            for(file in data.files){
+                let domain = (new URL(file.url))
+                domain.hostname = "eu2.contabostorage.com"
+                domain.pathname = "15a156a10fc44b3389695a25bae08894:foundry/"+domain.pathname
+                file.url = domain.toString()
+            }
+        }
+    }
+})
